316873d7a2ef479f9a744119289e0dd870181a90
ayebe51 <alayyubi612@gmail.com>
2026-01-19 10:56:41 +0700

    Remove leftover useEffect from YayasanApprovalPage

--- src/features/approval/YayasanApprovalPage.tsx
+++ src/features/approval/YayasanApprovalPage.tsx
@@ -71,14 +71,9 @@ export default function YayasanApprovalPage() {
   const [tanggalSuratMasuk, setTanggalSuratMasuk] = useState("")
   const [tahunAjaran, setTahunAjaran] = useState(calculateTahunAjaran())
   const [defaultKecamatan, setDefaultKecamatan] = useState("")
+  // Auto-update tahunAjaran when tanggalPenetapan changes
+  const currentTahunAjaran = tanggalPenetapan ? calculateTahunAjaran(tanggalPenetapan) : tahunAjaran
   // -------------------------
-
-  useEffect(() => {
-      if (tanggalPenetapan) {
-          const newAjaran = calculateTahunAjaran(tanggalPenetapan);
-          if (newAjaran) setTahunAjaran(newAjaran);
-      }
-  }, [tanggalPenetapan])
 
   const toRoman = (num: number): string => {
       const roman = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}

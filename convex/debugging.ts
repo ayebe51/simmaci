73dd4b0efbdc950f80b9f05e891ade4e0002df01
ayebe51 <alayyubi612@gmail.com>
2026-02-10 10:22:40 +0700

    fix: convex deployment error (added missing import)

--- convex/debugging.ts
+++ convex/debugging.ts
@@ -1,5 +1,4 @@
-
-import { query } from "./_generated/server";
+import { query, mutation } from "./_generated/server";
 import { v } from "convex/values";
 
 export const debugTeacherStats = query({

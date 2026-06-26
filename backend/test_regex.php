91107539fd8f5c5e6b8f82f1a817daefc8a7f79e
ayebe51 <alayyubi612@gmail.com>
2026-06-26 11:25:44 +0700

    Remove test scripts

--- backend/test_regex.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo preg_replace('/([a-zA-Z]{3,})(S\.|M\.|A\.Md\.|A\.Ma\.|Dr\.|Dra\.|Prof\.)/i', '$1 $2', 'BudiS.E.') . "\n";
-echo preg_replace('/([a-zA-Z]{3,})(S\.|M\.|A\.Md\.|A\.Ma\.|Dr\.|Dra\.|Prof\.)/i', '$1 $2', 'AhmadS.Pd.AUD.') . "\n";
-echo preg_replace('/([a-zA-Z]{3,})(S\.|M\.|A\.Md\.|A\.Ma\.|Dr\.|Dra\.|Prof\.)/i', '$1 $2', 'SitiDra.') . "\n";
-echo preg_replace('/([a-zA-Z]{3,})(S\.|M\.|A\.Md\.|A\.Ma\.|Dr\.|Dra\.|Prof\.)/i', '$1 $2', 'MUMBASITOHDra.') . "\n";

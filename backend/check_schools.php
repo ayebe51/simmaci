1c1903f35078ab1d7e8fab9a625dd917dc0a729a
ayebe51 <alayyubi612@gmail.com>
2026-07-06 11:06:26 +0700

    add check_schools.php

---
+++ backend/check_schools.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Models\School;
+
+echo "School 62: " . School::find(62)->nama . "\n";
+echo "School 72: " . School::find(72)->nama . "\n";
+echo "School 149: " . School::find(149)->nama . "\n";
